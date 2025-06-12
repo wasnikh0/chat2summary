8cbf69d0988004ba4b569a6a72d81ec2f0786350
Harsh Wasnik <wasnikh0@gmail.com>
2025-06-11 23:39:32 -0700

    Model Trainer

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
-#from src.textSummarizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerTrainingPipeline
-
+from src.textSummarizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerTrainingPipeline
 #from src.textSummarizer.pipeline.stage_4_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME="Data Ingestion stage"
--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -14,6 +14,7 @@ class DataTransformationConfig:
     data_path: Path
     tokenizer_name: Path
 
+@dataclass
 class ModelTrainerConfig:
     root_dir: Path
     data_path: Path
